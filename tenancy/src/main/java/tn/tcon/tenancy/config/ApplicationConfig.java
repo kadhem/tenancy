ff8604277651bcb9f3c503361b5256047a092efe
kkacem <kadhem.kacem@invationworld.com>
2017-05-25 22:53:07 +0100

    add multi tenancy

--- tenancy/src/main/java/tn/tcon/tenancy/config/ApplicationConfig.java
+++ tenancy/src/main/java/tn/tcon/tenancy/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package tn.tcon.tenancy.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages= "tn.tcon.tenancy")
